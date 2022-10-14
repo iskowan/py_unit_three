42c7a271fec6d3adb6ff07709505b1d42c38fdea
akiskowitz <iskowan@ssfs.org>
2022-10-14 15:30:52 -0400

    final_commit_2

--- assignment_three.py
+++ assignment_three.py
@@ -1,37 +1,36 @@
-#Anderson Iskowitz
-#Unit 3 Project
-#8 October 2022
-#This document draws a flower out depending on the user's input
+#Anderson Iskowitz Unit 3 Project
+#Last edited: 14 October 2022
+#This document draws a flower out depending on the user's input of size and color
+'''
+getting input for the length'''
 
-'''
-getting input for the length
-'''
 length = int(input("What is the length of the side?:"))
+
 '''getting the color of the center hexagon'''
 colorCenter = input("What is the color of the center hexagon?:")
+
 '''getting the color of the outer hexagons'''
 colorPetal = input("What is the color of the petals?:")
+
 '''making movement of the length twice'''
 lengthMovement = length * 2
 
 # defining shapes
+'''Defining hexagon shape and setting the color'''
 def drawHexagon(color):
     t.fillcolor(color)
-    '''
-    t.pencolor(color) makes the pen outline the same as the fill color
-    '''
     t.begin_fill()
     for x in range(6):
         t.fd(length)
         t.right(60)
     t.end_fill()
-
+'''defining the centeral hexagon and defining the color'''
 def drawCenter():
     t.rt(60)
     t.fd(length)
     t.left(60)
     drawHexagon(colorCenter)
-
+'''defining the total flower shape using the draw hexagon function'''
 def drawFlower():
     for x in range(6):
         t.penup()
@@ -40,16 +39,15 @@ def drawFlower():
         drawHexagon(colorPetal)
         t.rt(60)
 
-# defining main function
+'''defining main function'''
 def main():
     drawFlower()
     drawCenter()
 
-# importing turtle
+'''importing turtle'''
 import turtle
-# defining t to turtle so it is easier to type
+'''defining t to turtle so it is easier to type'''
 t = turtle.Turtle()
-
 # calling main function
 main()
 
